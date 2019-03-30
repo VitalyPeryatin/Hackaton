828932e5895eb402ca247ee145af0428d8f72745
Dmitry <kyloren228@gmail.com>
2019-03-30 23:46:28 +0300

    Change regexFuelCardName

--- app/src/main/java/com/infinity_coder/hackatonapp/RegEx.kt
+++ app/src/main/java/com/infinity_coder/hackatonapp/RegEx.kt
@@ -2,5 +2,5 @@ package com.infinity_coder.hackatonapp
 
 val regexBankCardName = "[A-Z]{4}\\s?[A-Z]{4}\\s?[A-Z]{4}\\s?[A-Z]{4}".toRegex()
 val regexHolderName = "[A-Z]+\\s([A-Z])+".toRegex()
-val regexFuelCardName = "\\d{10}".toRegex()
+val regexFuelCardName = "\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d".toRegex()
 val regexDate = "\\d{2}/\\d{2}".toRegex()
