eb81f043840e017c679bfbfc2e6b2921f873ffc6
Dmitry <kyloren228@gmail.com>
2019-03-30 23:53:56 +0300

    Fix regexBankCardName

--- app/src/main/java/com/infinity_coder/hackatonapp/RegEx.kt
+++ app/src/main/java/com/infinity_coder/hackatonapp/RegEx.kt
@@ -1,6 +1,6 @@
 package com.infinity_coder.hackatonapp
 
-val regexBankCardName = "[A-Z]{4}\\s?[A-Z]{4}\\s?[A-Z]{4}\\s?[A-Z]{4}".toRegex()
+val regexBankCardName = "\\d{3,4}\\s?\\d{4}\\s?\\d{4}\\s?\\d{3,4}".toRegex()
 val regexHolderName = "[A-Z]+\\s([A-Z])+".toRegex()
 val regexFuelCardName = "\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d".toRegex()
 val regexDate = "\\d{2}/\\d{2}".toRegex()
