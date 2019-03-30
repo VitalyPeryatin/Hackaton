dccece499ec1858ff4f01a98c07ef0243697e72c
yogg-soggot <nikit-iv777@ya.ru>
2019-03-30 12:55:38 +0300

    Ничего не работает, просто есть 2 активити

--- app/src/main/java/com/infinity_coder/hackatonapp/MainActivity.kt
+++ app/src/main/java/com/infinity_coder/hackatonapp/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.infinity_coder.hackatonapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        buttonGoScan.setOnClickListener(View.OnClickListener {
+            startActivity(Intent(applicationContext,ScanActivity::class.java))
+
+        })
     }
+
+
 }
---
+++ app/src/main/java/com/infinity_coder/hackatonapp/ScanActivity.kt
@@ -0,0 +1,81 @@
+package com.infinity_coder.hackatonapp
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.Matrix
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.otaliastudios.cameraview.Audio
+import com.otaliastudios.cameraview.CameraListener
+import com.otaliastudios.cameraview.Mode
+import com.otaliastudios.cameraview.PictureResult
+import kotlinx.android.synthetic.main.activity_scan.*
+import java.io.FileOutputStream
+import java.util.*
+
+class ScanActivity : AppCompatActivity() {
+
+    //потом передвинуть в файл с константами
+    val IMAGE_PATH_KEY = "imagePath"
+
+    private val maxBitmapWidth = 1500
+    private val maxBitmapHeight = 1500
+
+    private val cameraListener = object : CameraListener() {
+        override fun onPictureTaken(result: PictureResult) {
+            onPicture(result)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scan)
+        setCameraView()
+
+    }
+
+    private fun setCameraView(){
+        cameraView.setLifecycleOwner(this)
+        cameraView.mode = Mode.PICTURE
+        cameraView.audio = Audio.OFF
+        cameraView.addCameraListener(cameraListener)
+    }
+
+    private fun onPicture(result: PictureResult) {
+        result.toBitmap(maxBitmapWidth, maxBitmapHeight) { bitmap ->
+            if(bitmap != null) {
+                val matrix = Matrix()
+                matrix.postRotate(270f)
+                val verticalBitmap = if(bitmap.width > bitmap.height)
+                    Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+                else
+                    bitmap
+
+                val imagePath = saveBitmapToDir(verticalBitmap, "$filesDir")
+                val intent = Intent()
+                intent.putExtra(IMAGE_PATH_KEY, imagePath)
+                setResult(Activity.RESULT_OK, intent)
+            }
+            else{
+                setResult(Activity.RESULT_CANCELED, Intent())
+            }
+            finish()
+        }
+    }
+
+  private fun saveBitmapToDir(bitmap: Bitmap?, parentDir: String): String {
+        var out: FileOutputStream? = null
+        val pathName = "$parentDir/image-${UUID.randomUUID()}.png"
+        try {
+            out = FileOutputStream(pathName)
+            bitmap?.compress(Bitmap.CompressFormat.PNG, 100, out)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        } finally {
+            out?.close()
+        }
+        return pathName
+    }
+
+}
