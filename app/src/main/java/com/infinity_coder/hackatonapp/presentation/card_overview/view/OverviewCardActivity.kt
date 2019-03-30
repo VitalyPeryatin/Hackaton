576f1b851683a05da0059ffb2ccc932d2e9b5274
Vitaly <peryatin.vitalik37@gmail.com>
2019-03-30 19:53:51 +0300

    Change navigation

--- app/src/main/java/com/infinity_coder/hackatonapp/presentation/card_overview/view/OverviewCardActivity.kt
+++ app/src/main/java/com/infinity_coder/hackatonapp/presentation/card_overview/view/OverviewCardActivity.kt
@@ -1,16 +1,11 @@
 package com.infinity_coder.hackatonapp.presentation.card_overview.view
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.infinity_coder.hackatonapp.IMAGE_PATH_KEY
 import com.infinity_coder.hackatonapp.R
-import com.infinity_coder.hackatonapp.SCAN_REQUEST_CODE
-import com.infinity_coder.hackatonapp.presentation.scan.view.ScanActivity
-import com.squareup.picasso.Picasso
+import com.infinity_coder.hackatonapp.presentation.edit_card.view.EditCardActivity
 import kotlinx.android.synthetic.main.activity_overview_card.*
-import java.io.File
 
 
 class OverviewCardActivity: AppCompatActivity() {
@@ -18,17 +13,7 @@ class OverviewCardActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_overview_card)
         buttonGoScan.setOnClickListener {
-            startActivityForResult(Intent(this, ScanActivity::class.java),
-                SCAN_REQUEST_CODE
-            )
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if(requestCode == SCAN_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null){
-            Picasso.get()
-                .load(File(data.getStringExtra(IMAGE_PATH_KEY)))
-                .into(cardImage)
+            startActivity(Intent(this, EditCardActivity::class.java))
         }
     }
 }
--- app/src/main/java/com/infinity_coder/hackatonapp/presentation/edit_card/view/EditCardActivity.kt
+++ app/src/main/java/com/infinity_coder/hackatonapp/presentation/edit_card/view/EditCardActivity.kt
@@ -1,14 +1,21 @@
 package com.infinity_coder.hackatonapp.presentation.edit_card.view
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.infinity_coder.hackatonapp.IMAGE_PATH_KEY
 import com.infinity_coder.hackatonapp.R
+import com.infinity_coder.hackatonapp.SCAN_REQUEST_CODE
 import com.infinity_coder.hackatonapp.presentation.card_overview.view.OverviewCardActivity
 import com.infinity_coder.hackatonapp.presentation.scan.view.ScanActivity
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_edit_card.*
+import kotlinx.android.synthetic.main.activity_overview_card.*
+import java.io.File
 
 class EditCardActivity: AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +27,10 @@ class EditCardActivity: AppCompatActivity() {
         fabCapturePhoto.setOnClickListener {
             startActivity(Intent(this, ScanActivity::class.java))
         }
+
+        startActivityForResult(Intent(this, ScanActivity::class.java),
+            SCAN_REQUEST_CODE
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -37,5 +48,14 @@ class EditCardActivity: AppCompatActivity() {
             }
         }
         return true
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if(requestCode == SCAN_REQUEST_CODE && resultCode == Activity.RESULT_OK && data != null){
+            Toast.makeText(this, "Done", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            finish()
+        }
     }
 }
