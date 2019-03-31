0f9fe081850fcd04891838d6e4fbb36ed3ae0ecd
Dmitry <kyloren228@gmail.com>
2019-03-31 09:33:04 +0300

    Fixes

--- app/src/main/java/com/infinity_coder/hackatonapp/RegEx.kt
+++ app/src/main/java/com/infinity_coder/hackatonapp/RegEx.kt
@@ -1,6 +1,6 @@
 package com.infinity_coder.hackatonapp
 
-val regexBankCardName = "\\d{3,4}\\s?\\d{4}\\s?\\d{4}\\s?\\d{3,4}".toRegex()
+val regexBankCardName = "\"^(?:4[0-9]{12}(?:[0-9]{3})?|[25][1-7][0-9]{14}|6(?:011|5[0-9][0-9])[0-9]{12}|3[47][0-9]{13}|3(?:0[0-5]|[68][0-9])[0-9]{11}|(?:2131|1800|35\\\\d{3})\\\\d{11})\\\$\"".toRegex()
 val regexBankCardNameCheck = "\\d{4}\\s?\\d{4}\\s?\\d{4}\\s?\\d{4}".toRegex()
 val regexHolderName = "[A-Z]+\\s([A-Z])+".toRegex()
 val regexFuelCardName = "\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d\\s?\\d".toRegex()
